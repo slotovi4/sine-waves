944db6cd20d43ef1fcad85b573c2eed4755b3530
Роман Брунс <romanbruns@MacBook-Air-Roman.local>
2019-05-19 13:54:52 +0300

    update animation & added gui folders

--- dist/js/canvas.bundle.js
+++ dist/js/canvas.bundle.js
@@ -2653,24 +2653,46 @@ canvas.height = innerHeight;
 var wave = {
   y: canvas.height / 2,
   length: 0.01,
-  amplitude: 100
+  amplitude: 100,
+  frequency: 0.01
 };
 
-gui.add(wave, 'y', 0, canvas.height);
-gui.add(wave, 'length', -0.01, 0.01);
-gui.add(wave, 'amplitude', -300, 300);
+var strokeColor = {
+  h: 200,
+  s: 50,
+  l: 50
+};
+
+var waveFolder = gui.addFolder('wave');
+waveFolder.add(wave, 'y', 0, canvas.height);
+waveFolder.add(wave, 'length', -0.01, 0.01);
+waveFolder.add(wave, 'amplitude', -300, 300);
+waveFolder.add(wave, 'frequency', -0.01, 1);
+waveFolder.open();
+
+var strokeFolder = gui.addFolder('stroke');
+strokeFolder.add(strokeColor, 'h', 0, 255);
+strokeFolder.add(strokeColor, 's', 0, 255);
+strokeFolder.add(strokeColor, 'l', 0, 255);
+strokeFolder.open();
+
+var increment = wave.frequency;
 
 var animate = function animate() {
   requestAnimationFrame(animate);
+  c.fillStyle = 'rgba(0, 0, 0, 0.01)';
+  c.fillRect(0, 0, canvas.width, canvas.height);
 
   c.beginPath();
   c.moveTo(0, canvas.height / 2);
 
   for (var i = 0; i < canvas.width; i++) {
-    c.lineTo(i, wave.y + Math.sin(i * wave.length) * wave.amplitude);
+    c.lineTo(i, wave.y + Math.sin(i * wave.length + increment) * wave.amplitude);
   }
 
+  c.strokeStyle = 'hsl(200, 50%, 50%)';
   c.stroke();
+  increment += wave.frequency;
 };
 
 animate();
--- src/js/canvas.js
+++ src/js/canvas.js
@@ -10,24 +10,46 @@ canvas.height = innerHeight;
 const wave = {
   y: canvas.height / 2,
   length: 0.01,
-  amplitude: 100
+  amplitude: 100,
+  frequency: 0.01
 }
 
-gui.add(wave, 'y', 0, canvas.height);
-gui.add(wave, 'length', -0.01, 0.01);
-gui.add(wave, 'amplitude', -300, 300);
+const strokeColor = {
+  h: 200,
+  s: 50,
+  l: 50
+}
+
+const waveFolder = gui.addFolder('wave');
+waveFolder.add(wave, 'y', 0, canvas.height);
+waveFolder.add(wave, 'length', -0.01, 0.01);
+waveFolder.add(wave, 'amplitude', -300, 300);
+waveFolder.add(wave, 'frequency', -0.01, 1);
+waveFolder.open();
+
+const strokeFolder = gui.addFolder('stroke');
+strokeFolder.add(strokeColor, 'h', 0, 255);
+strokeFolder.add(strokeColor, 's', 0, 255);
+strokeFolder.add(strokeColor, 'l', 0, 255);
+strokeFolder.open();
+
+let increment = wave.frequency
 
 const animate = () => {
   requestAnimationFrame(animate);
+  c.fillStyle = 'rgba(0, 0, 0, 0.01)';
+  c.fillRect(0, 0, canvas.width, canvas.height);
 
   c.beginPath();
   c.moveTo(0, canvas.height / 2);
 
   for (let i = 0; i < canvas.width; i++) {
-    c.lineTo(i, wave.y + Math.sin(i * wave.length) * wave.amplitude);
+    c.lineTo(i, wave.y + Math.sin(i * wave.length + increment) * wave.amplitude);
   }
 
+  c.strokeStyle = 'hsl(200, 50%, 50%)';
   c.stroke();
+  increment += wave.frequency;
 }
 
 animate();
